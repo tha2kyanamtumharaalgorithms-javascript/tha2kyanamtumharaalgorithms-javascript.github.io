09ee92d33ad432fec3cd86c84c4afa6502008924
thakyanamtumhara <sales.dushirts@gmail.com>
2023-05-24 18:48:52 +0530

    n

--- aa1/exgst.js
+++ aa1/exgst.js
@@ -49,50 +49,63 @@ await mthdb(mth);
 })
 resolve();
 }).then(() => {
+    setTimeout(()=>{
+        let link1 = document.getElementById(mth);
+        let txt=link1.innerText;
+        let nm=txt+' '+(new Date().toLocaleTimeString("en-GB"))+'.csv';
+        let blob = new Blob([excsv1],{type: 'text/csv;charset=utf-8;'});
+        let url = URL.createObjectURL(blob);
+        alert(excsv1);
+        dfile(url,nm);
+    },1000)
     console.log(mth);
-    // pbar1.style.display='none';
-    let link1 = document.getElementById(mth);
-    let txt=link1.innerText;
-    // link1.href = dfile(excsv1, 'text/csv;encoding:utf-8')
-    // link1.download = txt+' '+(new Date().toLocaleTimeString("en-GB"))+'.csv';
-    link1.removeAttribute("onclick");
-    link1.classList.remove("w3-blue");
-    link1.classList.remove("w3-hover-purple");
-    link1.style.color='blue';
+    // // pbar1.style.display='none';
+    // let link1 = document.getElementById(mth);
+    // let txt=link1.innerText;
+   
+    // // link1.href = dfile(excsv1, 'text/csv;encoding:utf-8')
+    // // link1.download = txt+' '+(new Date().toLocaleTimeString("en-GB"))+'.csv';
+    // link1.removeAttribute("onclick");
+    // link1.classList.remove("w3-blue");
+    // link1.classList.remove("w3-hover-purple");
+    // link1.style.color='blue';
     // link1.click();
     // window.open(link1.href);
-    let nm=txt+' '+(new Date().toLocaleTimeString("en-GB"))+'.csv';
-    downloadBlob(excsv1, nm, 'text/csv;charset=utf-8;')
+    // let nm=txt+' '+(new Date().toLocaleTimeString("en-GB"))+'.csv';
+    // downloadBlob(excsv1, nm, 'text/csv;charset=utf-8;')
 
     }).catch((err)=>console.log(err))
 }
 
-function downloadBlob(content, filename, contentType) {
-    let blobx = new Blob([content], { type: contentType });
-    let urlx = URL.createObjectURL(blobx);
-    // let pomx = document.createElement('a');
-    // pomx.href = urlx;
-    // pomx.setAttribute('download', filename);
-    // pomx.click();
+function dfile(url,nm){
     new Promise(async(resolve, reject)=>{
-        let iframe = document.createElement("iframe");  document.body.appendChild(iframe);
-        let doc=iframe.contentWindow.document; 
-        doc.write(`<a id="link55" href="${urlx}" download="${filename}"></a>`);
-        doc.getElementById("link55").click();
-        resolve(iframe);
-        }).then((i) =>i.remove())
-    // dfile1(urlx,filename)
-  }
-  
-  function dfile1(link,name){
-    new Promise(async(resolve, reject)=>{
-    let iframe = document.createElement("iframe");  document.body.appendChild(iframe);
-    let doc=iframe.contentWindow.document; 
-    doc.write(`<a id="link55" href="${link}" download="${name}"></a>`);
-    doc.getElementById("link55").click();
-    resolve(iframe);
+    let htl=`<a id="link55" href="${url}" download="${nm}"></a>`;
+    let iframe = document.createElement("iframe");
+    document.body.appendChild(iframe);
+    let docx=iframe.contentWindow.document;
+    docx.open();
+    docx.write(htl);
+    docx.getElementById("link55").click();
+    docx.close();resolve(iframe);
     }).then((i) =>i.remove())
-    }
+  }
+
+// function downloadBlob(content, filename, contentType) {
+//     let blobx = new Blob([content], { type: contentType });
+//     let urlx = URL.createObjectURL(blobx);
+//     // let pomx = document.createElement('a');
+//     // pomx.href = urlx;
+//     // pomx.setAttribute('download', filename);
+//     // pomx.click();
+//     new Promise(async(resolve, reject)=>{
+//         let iframe = document.createElement("iframe");document.body.appendChild(iframe);
+//         let docx=iframe.contentWindow.document; 
+//         docx.write(`<a id="link55" href="${urlx}" download="${filename}"></a>`);
+//         docx.getElementById("link55").click();
+//         resolve(iframe);
+//         }).then((i) =>i.remove())
+//     // dfile1(urlx,filename)
+//   }
 // dfile(csv, 'text/csv;charset=utf-8;')
 // function dfile(content, contentType) {
 //     return URL.createObjectURL(new Blob([content], { type: contentType }))
