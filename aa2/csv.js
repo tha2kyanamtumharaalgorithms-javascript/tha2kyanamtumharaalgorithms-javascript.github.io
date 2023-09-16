b3229911cc78d56fd171526bad035a4ff7c1cf50
thakyanamtumhara <129951688+thakyanamtumhara@users.noreply.github.com>
2023-09-16 10:52:11 +0530

    Update csv.js

--- aa2/csv.js
+++ aa2/csv.js
@@ -181,3 +181,41 @@
 //   .catch((error) => {
 //     console.error('Error exporting data:', error);
 //   });
+
+
+const sourceDBName = 'party';
+const sourceObjectStoreName = 'pt';
+const data = [];
+const req = indexedDB.open(sourceDBName);
+
+req.onsuccess = function(event) {
+  const db = event.target.result;
+  const transaction = db.transaction([sourceObjectStoreName], 'readonly');
+  const objectStore = transaction.objectStore(sourceObjectStoreName);
+console.log('this is first step for json update');
+  objectStore.openCursor().onsuccess = function(event) {
+    const cursor = event.target.result;
+    if (cursor) {
+      data.push(cursor.value);
+      cursor.continue();
+     console.log('this is secound step for json update');
+    } else {
+      console.log(data);
+      const jsonData = JSON.stringify(data);
+      
+      // Create a Blob containing the JSON data
+      const blob = new Blob([jsonData], { type: 'application/json' });
+      
+      // Create a FileWriter to save the Blob to a file
+      window.webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024, function(fs) {
+       console.log('this is last step for json update');
+        fs.root.getFile('data.json', { create: true }, function(fileEntry) {
+          fileEntry.createWriter(function(fileWriter) {
+            fileWriter.write(blob);
+            console.log('JSON data saved as data.json in the current directory.');
+          });
+        });
+      });
+    }
+  };
+};
