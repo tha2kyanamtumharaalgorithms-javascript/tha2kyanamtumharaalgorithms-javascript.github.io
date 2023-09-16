353d674d702ed1673743063a03533848adbd49e2
thakyanamtumhara <129951688+thakyanamtumhara@users.noreply.github.com>
2023-09-16 10:45:06 +0530

    Update csv.js

--- aa2/csv.js
+++ aa2/csv.js
@@ -47,137 +47,137 @@
 
 
 
- document.getElementById('csvFileInput').addEventListener('change', function (e) {
-            const file = e.target.files[0];
-            if (file) {
-                const reader = new FileReader();
+//  document.getElementById('csvFileInput').addEventListener('change', function (e) {
+//             const file = e.target.files[0];
+//             if (file) {
+//                 const reader = new FileReader();
 
-                reader.onload = function (e) {
-                    const contents = e.target.result;
-                    processCSV(contents);
-                };
+//                 reader.onload = function (e) {
+//                     const contents = e.target.result;
+//                     processCSV(contents);
+//                 };
 
-                reader.readAsText(file);
-            }
-        });
+//                 reader.readAsText(file);
+//             }
+//         });
 
-        function processCSV(csv) {
-            const lines = csv.split('\n');
-            const jsonData = [];
+//         function processCSV(csv) {
+//             const lines = csv.split('\n');
+//             const jsonData = [];
 
-            const headers = lines[0].split(',');
-            for (let i = 1; i < lines.length; i++) {
-                const row = lines[i].split(',');
-                const rowData = {};
-                for (let j = 0; j < headers.length; j++) {
-                    rowData[headers[j]] = row[j];
-                }
-                jsonData.push(rowData);
-            }
+//             const headers = lines[0].split(',');
+//             for (let i = 1; i < lines.length; i++) {
+//                 const row = lines[i].split(',');
+//                 const rowData = {};
+//                 for (let j = 0; j < headers.length; j++) {
+//                     rowData[headers[j]] = row[j];
+//                 }
+//                 jsonData.push(rowData);
+//             }
 
-            displayData(jsonData);
-        }
+//             displayData(jsonData);
+//         }
 
-        function displayData(data) {
-            const jsonDataDisplay = document.getElementById('jsonDataDisplay');
-            jsonDataDisplay.textContent = JSON.stringify(data, null, 2);
+//         function displayData(data) {
+//             const jsonDataDisplay = document.getElementById('jsonDataDisplay');
+//             jsonDataDisplay.textContent = JSON.stringify(data, null, 2);
 
-            // Create a table and populate it with JSON data
-            const jsonTable = document.getElementById('jsonTable');
-            jsonTable.innerHTML = '';
-            const headers = Object.keys(data[0]);
-            const headerRow = jsonTable.insertRow(0);
-            headers.forEach(header => {
-                const cell = headerRow.insertCell(-1);
-                cell.innerHTML = header;
-            });
+//             // Create a table and populate it with JSON data
+//             const jsonTable = document.getElementById('jsonTable');
+//             jsonTable.innerHTML = '';
+//             const headers = Object.keys(data[0]);
+//             const headerRow = jsonTable.insertRow(0);
+//             headers.forEach(header => {
+//                 const cell = headerRow.insertCell(-1);
+//                 cell.innerHTML = header;
+//             });
 
-            data.forEach((rowData, index) => {
-                const row = jsonTable.insertRow(index + 1);
-                headers.forEach(header => {
-                    const cell = row.insertCell(-1);
-                    cell.innerHTML = rowData[header];
-                });
-            });
+//             data.forEach((rowData, index) => {
+//                 const row = jsonTable.insertRow(index + 1);
+//                 headers.forEach(header => {
+//                     const cell = row.insertCell(-1);
+//                     cell.innerHTML = rowData[header];
+//                 });
+//             });
 
-            // Show the popup
-            const popup = document.getElementById('popup');
-            popup.style.display = 'block';
-        }
+//             // Show the popup
+//             const popup = document.getElementById('popup');
+//             popup.style.display = 'block';
+//         }
 
-        // Function to close the popup
-        function closePopup() {
-            const popup = document.getElementById('popup');
-            popup.style.display = 'none';
-        }
+//         // Function to close the popup
+//         function closePopup() {
+//             const popup = document.getElementById('popup');
+//             popup.style.display = 'none';
+//         }
 
 
-// export dbs data from indexed db
-function getDataFromIndexedDB(dldb, dl) {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open(dbName);
+// // export dbs data from indexed db
+// function getDataFromIndexedDB(dldb, dl) {
+//   return new Promise((resolve, reject) => {
+//     const request = indexedDB.open(dbName);
 
-    request.onsuccess = (event) => {
-      const db = event.target.result;
-      const transaction = db.transaction(dl, 'readonly');
-      const objectStore = transaction.objectStore(dl);
-      const data = [];
+//     request.onsuccess = (event) => {
+//       const db = event.target.result;
+//       const transaction = db.transaction(dl, 'readonly');
+//       const objectStore = transaction.objectStore(dl);
+//       const data = [];
 
-      objectStore.openCursor().onsuccess = (e) => {
-        const cursor = e.target.result;
-        if (cursor) {
-          data.push(cursor.value);
-          cursor.continue();
-        } else {
-          resolve(data);
-        }
-      };
+//       objectStore.openCursor().onsuccess = (e) => {
+//         const cursor = e.target.result;
+//         if (cursor) {
+//           data.push(cursor.value);
+//           cursor.continue();
+//         } else {
+//           resolve(data);
+//         }
+//       };
 
-      transaction.onerror = (e) => {
-        reject(e.target.error);
-      };
-    };
+//       transaction.onerror = (e) => {
+//         reject(e.target.error);
+//       };
+//     };
 
-    request.onerror = (e) => {
-      reject(e.target.error);
-    };
-  });
-}
-function convertToCSV(data) {
-  const csv = [];
-  const header = Object.keys(data[0]);
-  csv.push(header.join(','));
+//     request.onerror = (e) => {
+//       reject(e.target.error);
+//     };
+//   });
+// }
+// function convertToCSV(data) {
+//   const csv = [];
+//   const header = Object.keys(data[0]);
+//   csv.push(header.join(','));
 
-  for (const item of data) {
-    const row = header.map((key) => item[key]);
-    csv.push(row.join(','));
-  }
+//   for (const item of data) {
+//     const row = header.map((key) => item[key]);
+//     csv.push(row.join(','));
+//   }
 
-  return csv.join('\n');
-}
-function exportCSV(csvData, fileName) {
-  const blob = new Blob([csvData], { type: 'text/csv' });
-  const url = URL.createObjectURL(blob);
+//   return csv.join('\n');
+// }
+// function exportCSV(csvData, fileName) {
+//   const blob = new Blob([csvData], { type: 'text/csv' });
+//   const url = URL.createObjectURL(blob);
 
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = fileName;
-  a.style.display = 'none';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+//   const a = document.createElement('a');
+//   a.href = url;
+//   a.download = fileName;
+//   a.style.display = 'none';
+//   document.body.appendChild(a);
+//   a.click();
+//   document.body.removeChild(a);
 
-  URL.revokeObjectURL(url);
-}
-const dbName = 'dldb';
-const storeName = 'dl';
-const fileName = 'exported_data.csv';
+//   URL.revokeObjectURL(url);
+// }
+// const dbName = 'dldb';
+// const storeName = 'dl';
+// const fileName = 'exported_data.csv';
 
-getDataFromIndexedDB(dbName, storeName)
-  .then((data) => {
-    const csvData = convertToCSV(data);
-    exportCSV(csvData, fileName);
-  })
-  .catch((error) => {
-    console.error('Error exporting data:', error);
-  });
+// getDataFromIndexedDB(dbName, storeName)
+//   .then((data) => {
+//     const csvData = convertToCSV(data);
+//     exportCSV(csvData, fileName);
+//   })
+//   .catch((error) => {
+//     console.error('Error exporting data:', error);
+//   });
