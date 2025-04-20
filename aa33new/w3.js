b3f6768c5dfc08b6dba1bef0190b2ee98109f22d
thakyanamtumhara <sales.dushirts@gmail.com>
2025-04-20 18:51:19 +0530

    fsdf

--- aa33new/w3.js
+++ aa33new/w3.js
@@ -45,3 +45,50 @@ async function sendd(urld, d9, b, q) {
       });
   })
 }
+
+async function expStock() {
+  let allod = []; let sss = Object.keys(selod5);
+  let csv = [];
+  if (sss.length) {
+    for (const [i, v] of sss.entries()) {
+      console.log(v, i); // ods2526043158848
+      csv.push(v);
+      await mthdb(selg.slice(-1) + v.slice(3, 9));
+      allod[i] = await oddb.od.get(Number(v.slice(3)));
+    }
+  }
+  // console.log(allod);
+  let all = {};
+  allod.forEach(v => {
+    let kk = v.od;
+    Object.keys(kk).forEach(t => {
+      all[t] = all[t] || {};
+      Object.keys(kk[t]).forEach(c => {
+        all[t][c] = all[t][c] || {};
+        Object.keys(kk[t][c]).forEach(s => {
+          all[t][c][s] = all[t][c][s] ? all[t][c][s] + kk[t][c][s] : kk[t][c][s];
+        });
+      });
+    });
+  });
+  // console.log(all);
+  Object.keys(all).forEach(t => {
+    Object.keys(all[t]).forEach(c => {
+      Object.keys(all[t][c]).forEach((s, i) => {
+        csv.push(`${t},${c},${s},${all[t][c][s]}`);
+      });
+    });
+  });
+  // console.log(csv);
+  let csvString = csv.join('\n');
+  let blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+  let url = URL.createObjectURL(blob);
+  let a = document.createElement('a');
+  a.href = url;
+  let t = new Date().toLocaleTimeString('en', { day: '2-digit', month: 'short', year: 'numeric', hour: "2-digit", minute: "2-digit", hour12: true });
+  a.download = 'export' + t.replaceAll(', ', '') + '.csv';
+  a.style.display = 'none';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
